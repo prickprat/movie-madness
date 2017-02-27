f6b5035537297de472bae3ebacd73908ba15775c
rattlesnake <ricky.ratnayake@gmail.com>
2017-02-28 09:33:59 +1100

    added tests for 6deg and routes for searching by string

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const https = require('https');
+// const https = require('https');
 const util = require('util');
+const requester = require('request-promise-native');
 
 const ENV_DEFS = require('./environment');
 const PORT = ENV_DEFS.PORT;
@@ -10,8 +11,31 @@ const API_BASE_URL = ENV_DEFS.API_BASE_URL;
 let app = express();
 
 app.get('/movie/search', (req, res) => {
-    console.log('API :: /movie ::');
-    res.json({});
+    console.log('API :: /movie/search ::');
+    queryString = req.query.query;
+    let api_url = `api_key=${API_KEY}&query=${queryString}`;
+    let apiRequestOpts = {
+        uri: `${API_BASE_URL}/search/movie/`,
+        qs: {
+            api_key: API_KEY,
+            query: queryString,
+        },
+        json: true,
+    };
+
+    requester(apiRequestOpts)
+        .then(apiRes => {
+            console.log('here');
+            if (apiRes.total_results > 0){
+                res.status(200).json(apiRes.results[0]);
+            } else {
+                res.status(200).json({});
+            }
+        })
+        .catch(err => {
+            console.log('there');
+            res.status(400).json(err);
+        });
 });
 
 app.get('/movie/id/:id', (req, res) => {
@@ -27,7 +51,7 @@ app.get('/movie/id/:id', (req, res) => {
     let api_url = `${API_BASE_URL}/movie/${id}?api_key=${API_KEY}`;
 
     console.log(`Requesting URL:${api_url} :: `);
-    https.get(api_url, (api_res) => {
+    require('https').get(api_url, (api_res) => {
         console.log(`api_res status : ${api_res.statusCode}`);
         let body = '';
 
--- index.test.js
+++ index.test.js
@@ -6,14 +6,57 @@ const request = require('supertest');
 const app = require('./index');
 
 describe('Movie Madness API', () => {
-    describe('Searching Movies', () => { 
-        it('should return json', (done) => {
-            request(app)
+    describe('Searching Movies', () => {
+        it('should return json and error for empty query', () => {
+            return request(app)
                 .get('/movie/search')
                 .set('Accept', 'application/json')
                 .expect('Content-Type', /json/)
-                .expect(200, done);
+                .expect(400);
+        }).timeout(5000);
+
+        it('should search by partial match string and return the first movie by default', () => {
+            return request(app)
+                .get('/movie/search?query=hunting')
+                .expect(200)
+                .then(res => {
+                    movie = res.body
+                    movie.id.should.equal(489);
+                    movie.original_title.should.match(/hunting/i);
+                });
+        }).timeout(5000);
+
+        it.skip('should be able to return up to a specified number of search results', () => {
+            let numMovies = 50;
+            return request(app)
+                .get(`/movie/search?date=2000&num_results=${numMovies}`)
+                .expect(200)
+                .then(res => {
+                    movies = res.body;
+                    movies.length.should.equal(numMovies);
+                });
         });
+    });
+
+    describe('6 Degrees of Kevin Bacon', () => {
+        it.skip('should provide the number of degrees of movie-separation for two given actors', () => {
+            let actor1 = '';
+            let actor2 = '';
+
+            //Get acted-in :: https://api.themoviedb.org/3/person/6384/movie_credits
+            //Get cast-of -- https://api.themoviedb.org/3/movie/561/credits
+
+
+            return request(app)
+                .get(`/complex/6deg?actor1=${actor1}&actor2=${actor2}`)
+                .expect(200)
+                .then(res => {
+                    results = res.body;
+                    results.degrees.should.equal();
+
+                });
+        });
+
     });
 
     describe('Movie Details', () => {
